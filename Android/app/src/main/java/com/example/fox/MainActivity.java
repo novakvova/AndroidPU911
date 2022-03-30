4d41d9ecff660800a437a7ee27e442f1b7b87335
novakvova <novakvova@gmail.com>
2022-03-30 09:35:33 +0300

    Upload image android get base64

--- Android/app/src/main/java/com/example/fox/MainActivity.java
+++ Android/app/src/main/java/com/example/fox/MainActivity.java
@@ -2,38 +2,103 @@ package com.example.fox;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.util.Base64;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.android.volley.toolbox.NetworkImageView;
 import com.example.fox.constants.Urls;
 import com.example.fox.network.ImageRequester;
 
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+
 public class MainActivity extends AppCompatActivity {
 
-    TextView txtInfo;
-    EditText editInfo;
     private ImageRequester imageRequester;
     private NetworkImageView myImage;
+    // constant to compare
+    // the activity result code
+    int SELECT_PICTURE = 200;
+
+
+    // One Preview Image
+    ImageView IVPreviewImage;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        txtInfo = findViewById(R.id.txtInfo);
-        editInfo = findViewById(R.id.editInfo);
 
         imageRequester = ImageRequester.getInstance();
         myImage = findViewById(R.id.myimg);
         String urlImg = Urls.BASE+"/images/1.jpg";
         imageRequester.setImageFromUrl(myImage, urlImg);
-
-
+        IVPreviewImage = findViewById(R.id.IVPreviewImage);
 
     }
-    public void onClickBtn(View view) {
-        String text = editInfo.getText().toString();
-        txtInfo.setText(text);
+
+
+    public void onSelectImage(View view) {
+        imageChooser();
     }
+
+    void imageChooser() {
+
+        // create an instance of the
+        // intent of the type image
+        Intent i = new Intent();
+        i.setType("image/*");
+        i.setAction(Intent.ACTION_GET_CONTENT);
+
+        // pass the constant to compare it
+        // with the returned requestCode
+        startActivityForResult(Intent.createChooser(i, "Select Picture"), SELECT_PICTURE);
+    }
+
+    // this function is triggered when user
+    // selects the image from the imageChooser
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (resultCode == RESULT_OK) {
+
+            // compare the resultCode with the
+            // SELECT_PICTURE constant
+            if (requestCode == SELECT_PICTURE) {
+                // Get the url of the image from data
+                Uri uri = data.getData();
+                //byte[] decodedStr = Base64.decode(selectedImageUri, Base64.DEFAULT);
+                if (null != uri) {
+                    // update the preview image in the layout
+                    IVPreviewImage.setImageURI(uri);
+
+                    Bitmap bitmap= null;
+                    try {
+                        bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(),uri);
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                    // initialize byte stream
+                    ByteArrayOutputStream stream=new ByteArrayOutputStream();
+                    // compress Bitmap
+                    bitmap.compress(Bitmap.CompressFormat.JPEG,100,stream);
+                    // Initialize byte array
+                    byte[] bytes=stream.toByteArray();
+                    // get base64 encoded string
+                    String sImage= Base64.encodeToString(bytes,Base64.DEFAULT);
+                    String app=sImage+"";
+                }
+            }
+        }
+    }
+
 }
